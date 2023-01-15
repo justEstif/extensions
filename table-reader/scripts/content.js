340b963f659fccb3a97913686ed27503ba52d69c
Estifanos <estifanosbeyene21@gmail.com>
2023-01-15 15:47:53 -0500

    feat: add table reader scripts

--- table-reader/scripts/content.js
+++ table-reader/scripts/content.js
@@ -1,5 +1,51 @@
 const table = document.querySelector(".ui-datatable-tablewrapper table");
 
+function getRows(table) {
+  return [...table.rows].map((row) =>
+    [...row.children].map((child) => child.innerText)
+  );
+}
+
+function createObject(rows) {
+  const headers = rows.shift();
+  return rows.map((row) => {
+    let object = {};
+    row.map((value, i) => {
+      object[headers[i]] = getBetterValue(headers[i], value);
+    });
+    return object;
+  });
+}
+
+function getBetterValue(initialHeader, initialValue) {
+  const header = initialHeader.split("");
+  const value = initialValue.split("");
+
+  if (header.every((letter) => value.includes(letter))) {
+    return initialValue.split("").splice(header.length).join("");
+  } else {
+    return initialValue;
+  }
+}
+
+function addToPage(obj) {
+  obj.map((elements) => {
+    let date = `${elements["Start Date"]} - ${elements["End Date"]}`;
+    let name = `${elements["Resource Name"]}`;
+
+    let dateParagraph = document.createElement("p");
+    dateParagraph.innerText = date;
+
+    let nameParagraph = document.createElement("p");
+    nameParagraph.innerText = name;
+
+    document.body.appendChild(dateParagraph);
+    document.body.appendChild(nameParagraph);
+  });
+}
+
 if (table) {
-  console.log(table);
+  const rows = getRows(table);
+  const tableAsObject = createObject(rows);
+  addToPage(tableAsObject);
 }
